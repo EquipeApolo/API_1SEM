faba611763d3b6e5ca54ff7fd72f5477786237b9
Equipe3-API <equipe3api2021@outlook.com>
2021-10-10 20:35:51 -0300

    Adicionado gifs.

--- automatização_clonagem.py
+++ automatização_clonagem.py
@@ -11,17 +11,16 @@ def nome_pasta(texto):
 
 
 count = 0
-with open(r"endereços_githib_api.txt") as f:
+with open(r"C:/Users/thale/Desktop/API/API_1SEM/endereços_github_api.txt") as f:
     for line in f:
         print("clonando endereço: ", line)
         remoteurl= line.rstrip("\n")
 
         nomePasta = nome_pasta(remoteurl)
         
-        localfolder="C:/python_teste_lucas6/"+nomePasta
+        localfolder=f"C:/Users/thale/Desktop/API/API_1SEM/teste_clonagem/" +nomePasta
 
         myrepo = git.Repo.clone_from(remoteurl, localfolder, env={"GIT_SSH_COMMAND": 'ssh -i C:/Users/jenny/.ssh/id_rsa'})
-        #myrepo.git.checkout("Equipe3-API/API_1SEM")
 
 
 
